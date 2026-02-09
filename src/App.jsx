75b463a9bab2546366c0b247d549f9e17a37d65f
anavitskiapeclixcom <53864647+Lyshka@users.noreply.github.com>
2026-02-09 18:09:45 +0300

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@ import gsap from "gsap";
 import About from "./components/About";
 import Hero from "./components/Hero";
 import { ScrollTrigger } from "gsap/all";
+import Navbar from "./components/Navbar";
 
 gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Navbar />
       <Hero />
-
       <About />
     </main>
   );
